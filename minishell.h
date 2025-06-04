a4a1152b936ba3ae6d38b69aef86f58fbd8bb198
rubenalba2003@gmail.com <rubenalba2003@gmail.com>
2025-06-04 19:55:35 +0200

    Created .h and main.c

---
+++ minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 16:42:27 by isastre-          #+#    #+#             */
+/*   Updated: 2025/06/04 17:20:34 by isastre-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <curses.h>
+#include <term.h>
+
+#endif MINISHELL_H
