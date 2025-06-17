60b23d6a997c38875a114897bcf97b98e3a05466
izhans <izhansastre@gmail.com>
2025-06-17 19:28:21 +0200

    implement prompt and command history

--- main.c
+++ main.c
@@ -6,11 +6,46 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 17:39:55 by isastre-          #+#    #+#             */
-/*   Updated: 2025/06/04 17:51:23 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/06/17 19:27:13 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+bool	ft_is_empty(char *string);
+
 int main(int argc, char **argv, char **envp)
 {
-	
+	if (argc > 1)
+		return (1);
+	char *line;
+	while (true)
+	{
+		// read -> is empty -> save
+		line = readline("minishell$");
+		printf("line: %s\n", line);
+
+		if (ft_is_empty(line))
+		{
+			free(line);
+			continue;
+		}
+		
+		add_history(line);
+		free(line);
+	}
+}
+
+bool	ft_is_empty(char *string)
+{
+	int		start;
+	int		end;
+
+	if (string == NULL)
+		return (true);
+	start = 0;
+	end = ft_strlen(string);
+	while (ft_isspace(string[start]) && string[start])
+		start++;
+	return (start == end);
 }
--- minishell.h
+++ minishell.h
@@ -6,27 +6,31 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 16:42:27 by isastre-          #+#    #+#             */
-/*   Updated: 2025/06/04 17:20:34 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/06/17 19:09:10 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <string.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-#include <curses.h>
-#include <term.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <string.h>
+# include <termios.h>
+# include <sys/ioctl.h>
+# include <curses.h>
+# include <term.h>
 
-#endif MINISHELL_H
+# include <stdbool.h>
+
+# include "libft/libft.h"
+
+#endif
