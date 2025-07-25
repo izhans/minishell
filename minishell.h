cd55fdf355ea8d52750742b3270b2006f0d50006
izhans <izhansastre@gmail.com>
2025-07-25 02:21:00 +0200

    update .h for using libft lists

--- minishell.h
+++ minishell.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 02:14:56 by isastre-          #+#    #+#             */
+/*   Updated: 2025/07/25 02:41:06 by isastre-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -26,49 +37,44 @@
 # define DOUBLE_COMMA '"'
 # define PIPE '|'
 
-typedef enum e_redir
+typedef struct s_line		t_line;
+typedef struct s_command	t_command;
+typedef struct s_redir		t_redir;
+typedef enum e_redir_type	t_redir_type;
+
+typedef struct s_minishell
+{
+	t_list	*envp;
+	int		exit_status; // last exit status
+	t_line	*line;
+}	t_minishell;
+
+typedef struct s_line
+{
+	char	*line;
+	int		cmd_number;
+	t_list	*cmds; // list of t_command wrappers
+}	t_line;
+
+typedef struct s_command // is contained into a t_list
+{
+	char	*path;
+	t_list	*args;
+	t_list	*redir; // list of t_redir wrappers
+}	t_command;
+
+typedef struct s_redir // is contained into a t_list
+{
+	t_redir_type	type;
+	char			*filename;
+}	t_redir;
+
+typedef enum e_redir_type
 {
 	INFILE=0,
 	OUTFILE=1,
 	OUTFILE_APPEND=2,
 	HERE_DOC=3
-}	t_redir;
-
-typedef struct s_list_str
-{
-	char				*content;
-	struct s_list_str	*next;
-}	t_list_str;
-
-typedef struct s_list_redir
-{
-	t_redir				type;
-	char				*filename;
-	struct s_list_redir	*next;
-}	t_list_redir;
-
-
-typedef struct s_command
-{
-	char					*path;
-	t_list_str				*args;
-	t_list_redir			*redir;
-	struct s_command		*next;
-}	t_command;
-
-
-typedef struct s_line
-{
-	char		*line;
-	int			cmd_number;
-	t_command	*cmds;
-}	t_line;
-
-typedef struct s_minishell
-{
-	char	**envp;
-	int		exit_status; // last exit status
-	t_line	*line;
-}	t_minishell;
+}	t_redir_type;
 
 #endif
