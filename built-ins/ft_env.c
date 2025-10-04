4e194b07ef577dcf9a2933570ae9806ea412c194
izhans <izhansastre@gmail.com>
2025-10-04 17:56:56 +0200

    fix ft_env to work with t_envp

--- built-ins/ft_env.c
+++ built-ins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 15:29:37 by isastre-          #+#    #+#             */
-/*   Updated: 2025/10/02 14:59:51 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/10/04 17:33:31 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	ft_env(t_minishell *mini)
 {
-	t_list	*current;
+	t_envp	*current;
 
-	current = mini->envp;
+	current = mini->tenvp;
 	while (current)
 	{
-		ft_putendl(current->content);
+		if (current->value != NULL)
+			printf("%s=%s\n", current->key, current->value);
 		current = current->next;
 	}
 	mini->exit_status = 0;
