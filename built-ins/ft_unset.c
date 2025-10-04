d04be35586106723d3560995bc88ab23ba4a57c9
izhans <izhansastre@gmail.com>
2025-10-04 21:33:15 +0200

    unset finished

--- built-ins/ft_unset.c
+++ built-ins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 20:39:41 by isastre-          #+#    #+#             */
-/*   Updated: 2025/10/04 21:07:14 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/10/04 21:31:43 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,18 @@
 void	ft_unset(t_minishell *mini, t_command *cmd)
 {
 	t_list	*args;
-	char	*key;
 
 	args = cmd->args->next;
 	mini->exit_status = 0;
 	while (args)
 	{
-		ft_process_arg(mini, key);
-		if (!ft_is_valid_identifier(key))
+		if (!ft_is_valid_identifier(args->content))
 		{
-			printf(BUILTIN_ERROR_IDENTIFIER_UNSET, key);
+			printf(BUILTIN_ERROR_IDENTIFIER_UNSET, (char *) args->content);
 			mini->exit_status = 1;
-			return ;
 		}
-		ft_remove_envp_var(mini->tenvp, ft_get_envp_var(mini->tenvp, key));
+		ft_remove_envp_var(&mini->tenvp,
+			ft_get_envp_var(mini->tenvp, args->content));
 		args = args->next;
 	}
 }
